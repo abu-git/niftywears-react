9df51b7f514ad8a863f0e6c4fc11e774a555d8e3
abu-git <abu.sammie@gmail.com>
2021-10-19 12:15:57 +0200

    about page done

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -1,6 +1,10 @@
-import { Container, Grid } from '@mui/material'
+import { Container, Grid, IconButton } from '@mui/material'
 import  { makeStyles } from '@mui/styles'
 import { styled } from '@mui/material/styles'
+import { Link } from 'react-router-dom'
+
+import { ArrowBack } from "@mui/icons-material"
+import Logo from "../assets/assets/favicon_io/favicon-32x32.png"
 
 import Footer from './Footer'
 
@@ -66,14 +70,34 @@ const Right = styled('div')(({theme}) => ({
 }))
 
 const useStyles = makeStyles({
-    header: {
-        width: "50%",
-        textAlign: "center",
-        alignContent: "center"
+    headerContainer: {
+        display: "flex",
+        justifyContent: "center",
+        //border: "1px solid red",
+        padding: 0,
+        margin: 0,
+        height: "4.2em"
     },
-    article: {
-        width: "50%",
-        textAlign: "center"
+    header: {
+        margin: 0,
+        padding: 0,
+        //border: "1px solid yellow"
+    },
+    h3effects: {
+        color: "white"
+    },
+    link: {
+        textDecoration: "none"
+    },
+    goback: {
+        display: "flex"
+    },
+    arrowback: {
+        color: "white"
+    },
+    backtext: {
+        color: "white",
+        paddingLeft: "1em"
     }
 })
 
@@ -89,14 +113,31 @@ export default function AboutPage(){
     return(
         <>
             <Container maxWidth="xl">
-                    <StyledContainer>
-                            <Left><h1>About Us</h1></Left>
-                            <Right>
-                                <p>We are a start up attire company with a simple business idea.
-                                Our aim is to procure and produce suitable clothing for anyone 
-                                willing to see value in our branded products.</p>
-                            </Right>
-                    </StyledContainer>
+                <div className={classes.headerContainer}>
+                    <Link to="/" className={classes.link}>
+                        <IconButton className={classes.header}>
+                            <img src={Logo} width="30px" height="30px" alt="Nifty Wears Logo" />
+                            <h3 className={classes.h3effects}>Nifty Wears</h3>
+                        </IconButton>
+                    </Link>
+                </div>
+                <hr />
+                {/* ---------------Go Back JSX----------- */}
+                <Link to="/" className={classes.link}>
+                    <div className={classes.goback}>
+                        <ArrowBack className={classes.arrowback} />
+                        <span className={classes.backtext}>Back to home</span>
+                    </div>
+                </Link>
+                {/*-----------------------------------------*/}
+                <StyledContainer>
+                        <Left><h1>About Us</h1></Left>
+                        <Right>
+                            <p>We are a start up attire company with a simple business idea.
+                            Our aim is to procure and produce suitable clothing for anyone 
+                            willing to see value in our branded products.</p>
+                        </Right>
+                </StyledContainer>
             </Container>
             <Footer />
         </>
