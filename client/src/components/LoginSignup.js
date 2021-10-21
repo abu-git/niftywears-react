d640cd9d43ca5c917119056b526f1b3e658b422c
abu-git <abu.sammie@gmail.com>
2021-10-22 01:43:08 +0200

    login input functionality started...not yet wworking

--- client/src/components/LoginSignup.js
+++ client/src/components/LoginSignup.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import  { makeStyles } from '@mui/styles'
 import { Dialog, DialogActions, DialogContent, DialogContentText } from '@mui/material'
 import { DialogTitle, TextField } from '@mui/material'
+import validator from 'validator'
 
 const useStyles = makeStyles({
     loginsignup: {
@@ -53,6 +54,33 @@ export default function LoginSignup(){
 
     //Dialog Login config
     const [openLogin, setOpenLogin] = useState(false)
+    const [loginEmail, setLoginEmail] = useState('')
+    const [loginPassword, setLoginPassword] = useState('')
+    const [loginHelperText, setLoginHelperText] = useState('Enter Email')
+
+    const validateLoginInput = () => {
+        //email text field
+        if(validator.isEmpty(loginEmail)){
+            setLoginHelperText('Email field is required')
+            return false
+        }else if(!validator.isEmail(loginEmail)){
+            setLoginHelperText('Email is invalid')
+            return false
+        }else if(validator.isEmpty(loginPassword)){
+            setLoginHelperText('Password field is required')
+            return false
+        }
+        return true
+    }
+
+    const handleLoginSubmit = (e) => {
+        e.preventDefault()
+        const validate = validateLoginInput()
+        console.log(validate + " :is validate value")
+
+        console.log(loginEmail + " :is Email value")
+
+    }
 
     const handleLoginOpen = () => {
         setOpenLogin(true)
@@ -82,41 +110,47 @@ export default function LoginSignup(){
             </nav>
 
             {/* Login Dialog */}
-            <Dialog open={openLogin} onClose={handleLoginClose}>
-                <div className={classes.dialogColor}>
-                <DialogTitle>
-                    <h3>Login</h3>
-                </DialogTitle>
-                <DialogContent>
-                    <TextField
-                        id="email"
-                        label="email"
-                        type="email"
-                        margin="dense"
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                    />
-                    <TextField 
-                        id="password"
-                        label="password"
-                        type="password"
-                        margin="dense"
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                    />                   
-                </DialogContent>
-                <DialogActions>
-                    <button onClick={handleLoginClose} className={classes.button}>
-                        <h4 className={classes.h4}>Cancel</h4>
-                    </button>
-                    <button className={classes.button}>
-                        <h4 className={classes.h4}>Log In</h4>
-                    </button>
-                </DialogActions>
-                </div>
-            </Dialog>
+            <form onSubmit={handleLoginSubmit}>
+                <Dialog open={openLogin} onClose={handleLoginClose}>
+                    <div className={classes.dialogColor}>
+                    <DialogTitle>
+                        Login
+                    </DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            id="loginEmail"
+                            label="email"
+                            type="email"
+                            margin="dense"
+                            variant="outlined"
+                            size="small"
+                            value={loginEmail}
+                            helperText={loginHelperText}
+                            onChange={(e) => setLoginEmail(e.target.value)}
+                            fullWidth
+                        />
+                        <TextField 
+                            id="password"
+                            label="password"
+                            type="password"
+                            margin="dense"
+                            variant="outlined"
+                            size="small"
+                            value={loginPassword}
+                            onChange={(e) => setLoginPassword(e.target.value)}
+                            fullWidth
+                        />                   
+                    </DialogContent>
+                    <DialogActions>
+                        <button onClick={handleLoginClose} className={classes.button}>
+                            <h4 className={classes.h4}>Cancel</h4>
+                        </button>
+                        <input className={classes.button} type="submit" value="Log In" onSubmit={handleLoginSubmit}/>
+                    </DialogActions>
+                    </div>
+                </Dialog>
+            </form>
+
 
             {/* Sign Up Dialog */}
             <Dialog open={openSignup} onClose={handleSignupClose}>
